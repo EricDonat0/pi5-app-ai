8c01728469c651b4780c019cd6c9a9e45d400caf
EricDonat0 <ericdonato09@gmail.com>
2026-04-21 18:30:05 -0300

    Configurando a estrutura inicial do React Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,20 @@
+import { BrowserRouter, Routes, Route} from "react-router";
 import { useState } from "react";
+import { AppLayout } from "./app-layout";
+import { HomePage } from "@/routes/home-page";
+import { WatchListPage } from "@/routes/watch-page";
+
 
 export function App () {
-    const [counter, setCounter] = useState(0);
-
     return (
-        <div>
-            <h1>Hello, Vite + React!</h1>
-            <p>Só para ver se isso funciona mesmo...</p>
-            <hr />
-            <p>Contador: {counter}</p>
-            <button onClick={() => setCounter(counter + 1)}>+1</button>
-            <button onClick={() => setCounter(counter - 1)}>-1</button>
-        </div>
+        <BrowserRouter>
+            <AppLayout>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/watch" element={<WatchListPage />} />
+                    
+                </Routes>
+            </AppLayout>
+        </BrowserRouter>
     );
 }
